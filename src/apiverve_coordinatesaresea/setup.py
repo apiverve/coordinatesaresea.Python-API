30618fed4cf45a6fb29288132c197cf02289563d
APIVerve Team <hello@apiverve.com>
2025-03-28 17:09:46 -0500

    Bug Fixes

--- src/apiverve_coordinatesaresea/setup.py
+++ src/apiverve_coordinatesaresea/setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup, find_packages
 
+import os
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirements_file = os.path.join(lib_folder, 'requirements.txt')
+install_requires = ["requests >= 2.25.1", "setuptools >= 56.0.0"]
+if os.path.exists(requirements_file):
+    with open(requirements_file, 'r') as f:
+        install_requires = f.read().splitlines()
+
 setup(
     name='apiverve_coordinatesaresea',
-    version='1.0.9',
+    version='1.1.9',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'requests',
-        'setuptools'
-    ],
+    install_requires=install_requires,
     description='Coordinates are Sea is a simple tool for checking if coordinates are in the sea. It returns if the coordinates are in the sea or not.',
     author='APIVerve',
     author_email='hello@apiverve.com',
